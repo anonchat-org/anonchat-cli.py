e87ea7e8e0a6b6f3a0323957fe2723c6be0fd8f7
KrutosX <52695559+KrutosVIP@users.noreply.github.com>
2022-02-03 19:16:34 +0400

    Add files via upload

---
+++ README.md
@@ -0,0 +1,7 @@
+# anonchat-cli.py
+Python implementation of anonchat CLI
+
+## Usage
+```
+$ python3 main.py <ip:port> [-n Nickname]
+```
---
+++ main.py
@@ -0,0 +1,75 @@
+import argparse, sys, socket
+import multiprocessing
+import traceback
+
+class Client:
+    def __init__(self):
+        self.messages = []
+        
+        self.parser()
+        self.start()
+
+    def parser(self):
+        parser = argparse.ArgumentParser(
+        prog="anonchat-cli",
+        description = "Connect to the anonchat",
+        epilog="---- Oh, hello there!")
+ 
+        parser.add_argument("ip", help = "IP of anonchat-server", type=str)
+        parser.add_argument("-n", "--nick", help = "Your nick", type=str, dest = "nick", default="Anon")
+        
+        args = parser.parse_args()
+
+        self.nick = args.nick
+
+        ip = args.ip.split(":")
+        ip.append(6969)
+
+        self.ip = ip[0]
+        try:
+            self.port = int(ip[1])
+        except:
+            print(f"Cannot parse port {ip[1]} as number. Aborting.")
+            sys.exit()
+
+    def start(self):
+        
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.socket.connect((self.ip, self.port))
+
+        self.request = multiprocessing.Process(target=self.mp_request, args=())
+        self.request.start()
+        try:
+            while True:
+                message = input("")
+                if message.strip():
+                    self.socket.send((f"<{self.nick}> " + message).encode())
+        except:
+            traceback.print_exc()
+            self.request.terminate()
+            self.socket.close()
+            sys.exit()
+
+    def mp_request(self):
+        while True:
+            if self.socket.fileno() != -1:
+                try:
+                    message = self.socket.recv(1024)
+                except:
+                    break
+
+            if not message:
+                break
+            
+            try:
+                message = message.decode()
+            except:
+                self.messages.append("Message was recieved, but the contents cannot be decoded :(")
+            else:
+                self.messages.append(message.strip())
+            print("> " + str(self.messages[-1]), end="\n", flush=True)
+
+if __name__ == "__main__":
+    cli = Client()
+        
+        
